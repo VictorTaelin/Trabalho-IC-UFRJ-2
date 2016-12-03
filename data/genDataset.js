09da317f0ad2ea6c4e9602a7af90aa1c738e5fc6
Victor Hernandes Silva Maia <srvictormaia@gmail.com>
2016-12-03 16:18:46 -0200

    Initial commit

---
+++ data/genDataset.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+var csv = fs.readFileSync("pokemon.csv", "utf8");
+
+var head = csv.split("\n")[0];
+var pokes = csv.split("\n").slice(1).map(function(line){
+  vars = line.split(",");
+  var weight = Number(vars[16].slice(0,vars[16].indexOf(" ")));
+  var ft_in = vars[17].replace(/[^\d']/g,'').split("'").map(Number);
+  var height = (ft_in[0] + ft_in[1]/12)*0.3048;
+  return {
+    name: vars[3],
+    type: vars[4],
+    hp: Number(vars[9]),
+    atk: Number(vars[10]),
+    def: Number(vars[11]),
+    spa: Number(vars[12]),
+    spd: Number(vars[13]),
+    spe: Number(vars[14]),
+    tot: Number(vars[15]),
+    weight: weight*0.453592,
+    height: height
+  };
+});
+fs.writeFileSync("dataset.csv", 
+  "name,type,hp,atk,def,spa,spd,spe,weight,height\n"
+  + pokes.map(function(poke){
+    return [poke.name, poke.type, poke.hp, poke.atk,
+      poke.def, poke.spa, poke.spd, poke.spe,
+      poke.weight, poke.height].join(",");
+  }).join("\n"))
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Introdução
+
+O objetivo deste trabalho é tentar prever o tipo de um Pokémon baseado em seus atributos.
+
+## Dataset
+
+O dataset utilizado foi obtido do site [Kaggle](https://www.kaggle.com/mylesoneill/d/mylesoneill/pokemon-sun-and-moon-gen-7-stats/new-sun-and-moon-pokemon-analysis). 
